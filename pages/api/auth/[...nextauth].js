cda5de6ca4088b30f0990813632ae32605b73899
Henrik Næss <henrik@heka.no>
2020-12-06 22:57:17 +0100

    Use auth0 as next-auth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,17 +3,22 @@ import Providers from 'next-auth/providers'
 
 const options = {
   providers: [
-    Providers.Email({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASS
-        }
-      },
-      from: process.env.EMAIL_FROM
-    }),
+    /*    Providers.Email({
+          server: {
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            auth: {
+              user: process.env.EMAIL_SERVER_USER,
+              pass: process.env.EMAIL_SERVER_PASS
+            }
+          },
+          from: process.env.EMAIL_FROM
+        }),*/
+    Providers.Auth0({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      domain: process.env.AUTH0_DOMAIN
+    })
   ],
   database: process.env.DATABASE_URL,
 }
